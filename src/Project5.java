d2b917e4bba0937df777f69bdfa4a14c6e97311f
Clarissa <clarissafuller5@yahoo.com>
2026-02-09 20:28:11 -0500

    creates loop and creates ISBN to finish project

--- src/Project5.java
+++ src/Project5.java
@@ -22,15 +22,31 @@ public class Project5 {
         int sum = 0;
 
         // loop through each character in the string (index 0 to 11)
-        // convert the current character to an integer digit
-        // if index is even, add digit * 1 to sum
-        // if index is odd, add digit * 3 to sum
+        for (int i = 0; i < isbn.length(); i++) {
+
+            // convert the current character to an integer digit
+            int digit = isbn.charAt(i) - '0';
+
+            // if index is even, add digit * 1 to sum
+            if (i % 2 == 0) {
+                sum += digit;
+            }
+            // if index is odd, add digit * 3 to sum
+            else {
+                sum += digit * 3;
+            }
+        }
 
         // compute checksum using:
         // checksum = 10 - (sum % 10)
+        int checksum = 10 - (sum % 10);
 
         // if checksum is 10, replace it with 0
+        if (checksum == 10) {
+            checksum = 0;
+        }
 
         // display the full ISBN-13 number (input + checksum)
+        System.out.println("The ISBN-13 number is " + isbn + checksum);
     }
 }
