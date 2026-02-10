85554b17f4ebf3d7dd4158322032f6e08dcf689c
Clarissa <clarissafuller5@yahoo.com>
2026-02-09 19:44:41 -0500

    comment plan for project

---
+++ src/Project1.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+public class Project1 {
+    // declare counters for positives and negatives
+    // declare a variable for total (sum)
+    // declare a variable for count of non-zero numbers
+
+    // prompt the user for input
+
+    // loop: read integers until the user enters 0
+    // if number > 0, increment positive counter
+    // if number < 0, increment negative counter
+    // add number to total
+    // increment count
+
+    // if count is 0
+    // display "No numbers are entered except 0"
+    // else
+    // compute average as a floating-point value
+    // display positives, negatives, total, and average
+}
